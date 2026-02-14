c9fc3f32d92096a5f238a28ca6785c150a7b5931
jhrchicken <jhrchicken@users.noreply.github.com>
2026-02-14 23:56:45 +0900

    Lv1 : 콜라 문제

---
+++ src/Lv1_콜라문제.java
@@ -0,0 +1,12 @@
+public class Lv1_콜라문제 {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+
+        while (n >= a) {
+            answer += n / a * b;
+            n = (n / a * b) + (n % a);
+        }
+
+        return answer;
+    }
+}
