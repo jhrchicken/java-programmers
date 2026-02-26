31ae088fbc616fd9edfe8f650c2fc745317efc75
jhrchicken <jhr.chicken@gmail.com>
2026-02-26 18:13:08 +0900

    Lv2 : 부모의 형질을 모두 가지는 대장균 찾기

---
+++ src/Lv2_부모의형질을모두가지는대장균찾기.sql
@@ -0,0 +1,6 @@
+SELECT child.id, child.genotype, parent.genotype AS parent_genotype
+FROM ecoli_data child
+JOIN ecoli_data parent
+ON child.parent_id = parent.id
+WHERE (child.genotype & parent.genotype) = parent.genotype
+ORDER BY child.id;
