fc205d9ffc08a40442dfe9c6fe5a6499cbccc676
jhrchicken <jhr.chicken@gmail.com>
2026-02-26 17:03:03 +0900

    Lv1 : 가장 큰 물고기 10마리 구하기

--- src/Lv2_조건에맞는개발자찾기.sql
+++ src/Lv2_조건에맞는개발자찾기.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT d.id, d.email, d.first_name, d.last_name
 FROM developers d
-JOIN skillcodes d
+JOIN skillcodes s
 ON (d.skill_code & s.code) = s.code
-WHERE S.name IN ('Python', 'C#')
+WHERE s.name IN ('Python', 'C#')
 ORDER BY d.id;
